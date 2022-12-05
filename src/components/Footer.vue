03e550a798dde015d02bef034c5d5d59a1b602b5
Matias-15 <mcriado.cg@gmail.com>
2022-12-04 22:19:32 -0300

    Add header and footer app and signOut button when user is logged in app

---
+++ src/components/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+  <footer class="bg-indigo-50">
+    <div class="container px-4 2xl:px-14 mx-auto py-6">
+      <div class="flex flex-col items-center gap-1 md:flex-row md:justify-between">
+        <p class="text-font-color lg:text-lg">Dirrección: <span class="font-bold ">Av. Matta #456</span></p>
+        <p class="text-font-color lg:text-lg">Todos los derechos reservados ©</p>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+  
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,37 @@
+<script setup>
+  import { useAuthStore } from '../stores/auth';
+
+  const store = useAuthStore();
+
+  const logOut = () => {
+    store.signOutUser()
+  };
+</script>
+
+<template>
+  <header class="bg-indigo-100 py-4 relative">
+    <nav class="md:flex md:justify-between md:items-center container px-4 2xl:px-14 mx-auto">
+      <div class="logo">
+        <img src="src/assets/logo-neumaticos-tr.png" alt="Logo Neumaticos a tu medida">
+      </div>
+      <!-- <span id="btn-menu" class="cursor-pointer lg:hidden"><img src="src/assets/menu.svg" alt="Menú mobile"></span> -->
+      <ul id="navigation-menu" class="pt-5 pb-3 md:pt-0 md:pb-0 md:flex md:items-center transition-all ease-in duration-500">
+        <li class="mx-2 mb-2 md:mx-1 md:mb-0 md:inline-flex"><RouterLink to="/" class="md:p-3 hover:bg-indigo-500 hover:text-white duration-500 rounded-lg">Home</RouterLink> </li>
+        <li v-if="!store.isLogged" class="mx-2 mb-2 md:mx-1 md:mb-0 md:inline-flex"><RouterLink to="/login" class="md:p-3 hover:bg-indigo-500 hover:text-white duration-500 rounded-lg">Login</RouterLink> </li>
+        <li class="mx-2 mb-4 md:mx-1 md:mb-0 md:inline-flex"><RouterLink to="/dashboard" class="md:p-3 hover:bg-indigo-500 hover:text-white duration-500 rounded-lg">Dashboard</RouterLink></li>
+        
+        <li v-if="store.isLogged" class="mx-2 md:mx-0 md:ml-5">
+          <button @click="logOut" class="px-4 py-3 bg-button rounded-lg duration-500 hover:bg-indigo-500">Cerrar Sesión</button>
+        </li>
+      </ul>
+      
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+.logo img {
+    width: 250px;
+    border-radius: 20px;
+  }
+</style>
